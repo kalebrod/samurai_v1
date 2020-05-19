93e42aa5ce9153216bda32e2d4d9135457441f77
Kaleb Rodrigues <kaleb-rod@outlook.com>
2020-05-18 21:05:47 -0300

    setup

--- samurai_app.py
+++ samurai_app.py
@@ -1,7 +1,18 @@
-from flask import Flask
-app = Flask(__name__)
+from decouple import config as config_decouple
 
-@app.route("/")
-@app.route("index")
-def index():
-    return "Hello World"
+def create_app(enviroment):
+    app = Flask(__name__)
+
+    app.config.from_object(enviroment)
+
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
+    return app
+
+enviroment = config['development']
+if config_decouple('PRODUCTION', default=False):
+    enviroment = config['production']
+
+app = create_app(enviroment)
