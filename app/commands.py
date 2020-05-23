3ea3fbb9aebe2f3abffaaf51ad2148f33185bf89
Kaleb Rodrigues <kaleb-rod@outlook.com>
2020-05-22 21:18:20 -0300

    Setup

--- app/commands.py
+++ app/commands.py
@@ -7,4 +7,6 @@ from .models import User
 @click.command(name='create_tables')
 @with_appcontext
 def create_tables():
+    print("Creating tables.....")
     db.create_all()
+    print("Tables created.....")
